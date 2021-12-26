f31de4a23cce6ef759b9170ea8c16d79bfb29a47
Winme <winme0308@gmail.com>
2021-12-26 16:58:45 +0800

    add helper function getCurrentTextSelection

---
+++ src/utils/getCurrentTextSelection.ts
@@ -0,0 +1,13 @@
+import { EditorState } from 'draft-js';
+
+export const getCurrentTextSelection = (editorState: EditorState): string => {
+  const selectionState = editorState.getSelection();
+  const anchorKey = selectionState.getAnchorKey();
+  const currentContent = editorState.getCurrentContent();
+  const currentContentBlock = currentContent.getBlockForKey(anchorKey);
+  const start = selectionState.getStartOffset();
+  const end = selectionState.getEndOffset();
+  const selectedText = currentContentBlock.getText().slice(start, end);
+
+  return selectedText;
+};
